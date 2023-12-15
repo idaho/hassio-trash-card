2765e49a1db4788f4c044ad5620c64d40e1bc12f
idaho <664101+idaho@users.noreply.github.com>
2023-12-15 13:40:22 +0100

    fix: adjust link of help icon in editor card (#111)

---
+++ src/utils/registerCustomCard.ts
@@ -0,0 +1,28 @@
+import { repository } from '../../package.json';
+
+interface RegisterCardParams {
+  type: string;
+  name: string;
+  description: string;
+}
+const registerCustomCard = (params: RegisterCardParams) => {
+  const windowWithCards = window as unknown as Window & {
+    customCards: unknown[];
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  windowWithCards.customCards = windowWithCards.customCards || [];
+
+  // Const cardPage = params.type.replace('-card', '').replace('mushroom-', '');
+
+  windowWithCards.customCards.push({
+    ...params,
+    preview: true,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    documentationURL: `${repository.url}/blob/main/README.md`
+  });
+};
+
+export {
+  registerCustomCard
+};
