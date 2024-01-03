b41b6bb7b3c7336ba9ca04def5c15c3b2db9d0a9
idaho <664101+idaho@users.noreply.github.com>
2024-01-03 09:36:47 +0100

    add italian language (#155)
    * add italian language
    
    * fix linter error

--- src/localize.ts
+++ src/localize.ts
@@ -3,13 +3,15 @@ import type { HomeAssistant } from 'lovelace-mushroom/src/ha';
 import * as de from './translations/de.json';
 import * as en from './translations/en.json';
 import * as fr from './translations/fr.json';
+import * as it from './translations/it.json';
 import * as sk from './translations/sk.json';
 
 const languages: Record<string, unknown> = {
   de,
+  en,
   fr,
-  sk,
-  en
+  it,
+  sk
 };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
