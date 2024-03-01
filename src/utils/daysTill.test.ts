6e3b0648ddc4ea19293e5ba73ddc5f26585d7b45
idaho <664101+idaho@users.noreply.github.com>
2024-03-01 20:13:41 +0100

    fix days till calculation when event is not whole day event (#243)

---
+++ src/utils/daysTill.test.ts
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { daysTill } from './daysTill';
+
+import type { CalendarItem } from './calendarItem';
+
+describe('daysTill', (): void => {
+  test('2 days till whole day events', async () => {
+    const date = new Date(`1970-01-02T00:00:00.000`);
+
+    const item = {
+      date: {
+        start: new Date(`1970-01-04T00:00:00.000`)
+      }
+    } as CalendarItem;
+
+    const result = daysTill(date, item);
+
+    expect(result).toEqual(2);
+  });
+
+  test('2 days till event at the evening', async () => {
+    const date = new Date(`1970-01-02T00:00:00.000`);
+
+    const item = {
+      date: {
+        start: new Date(`1970-01-04T22:00:00.000`)
+      }
+    } as CalendarItem;
+
+    const result = daysTill(date, item);
+
+    expect(result).toEqual(2);
+  });
+
+  test('2 days till event during leap ear', async () => {
+    const date = new Date(`2024-02-28T00:00:00.000`);
+
+    const item = {
+      date: {
+        start: new Date(`2024-03-01T00:00:00.000`)
+      }
+    } as CalendarItem;
+
+    const result = daysTill(date, item);
+
+    expect(result).toEqual(2);
+  });
+
+  describe('calculate correct during time changeover', (): void => {
+    test('from normal to day light saving', async () => {
+      const date = new Date(`2024-03-30T00:00:00.000`);
+
+      const item = {
+        date: {
+          start: new Date(`2024-04-01T00:00:00.000`)
+        }
+      } as CalendarItem;
+
+      const result = daysTill(date, item);
+
+      expect(result).toEqual(2);
+    });
+
+    test('from day light saving to normal', async () => {
+      const date = new Date(`2024-10-26T00:00:00.000`);
+
+      const item = {
+        date: {
+          start: new Date(`2024-10-29T00:00:00.000`)
+        }
+      } as CalendarItem;
+
+      const result = daysTill(date, item);
+
+      expect(result).toEqual(3);
+    });
+  });
+});
