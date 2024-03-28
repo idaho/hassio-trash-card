76cfefbe263e3d19162d431cba4511c15a6d99db
idaho <664101+idaho@users.noreply.github.com>
2024-03-28 13:41:17 +0100

    timezone calculation half hours timezones (#276)

--- src/utils/getTimeZoneOffset.ts
+++ src/utils/getTimeZoneOffset.ts
@@ -1,14 +1,15 @@
-const getTimeZoneOffset = (): string => {
-  const now = new Date();
-
+const getTimeZoneOffset = (now: Date = new Date()): string => {
   const offset = -now.getTimezoneOffset();
+
   const hours = Number.parseInt(`${offset / 60}`, 10);
 
   const formattedHours = hours < 0 ?
     `-${`${Number.parseInt(`${offset / 60}`, 10) * -1}`.padStart(2, '0')}` :
     `${Number.parseInt(`${offset / 60}`, 10)}`.padStart(2, '0');
 
-  return `${offset >= 0 ? '+' : ''}${formattedHours}:${`${offset % 60}`.padStart(2, '0')}`;
+  const minutes = offset % 60;
+
+  return `${offset >= 0 ? '+' : ''}${formattedHours}:${`${minutes < 0 ? minutes * -1 : minutes}`.padStart(2, '0')}`;
 };
 
 export {
