58ff5b87f6a6eac8d99c2ef5b731b3e551e06492
idaho <664101+idaho@users.noreply.github.com>
2024-10-07 08:48:17 +0200

    fix date time calculation caused by day light saving (different timezone offset during the year)

--- src/utils/normaliseEvents.ts
+++ src/utils/normaliseEvents.ts
@@ -1,6 +1,8 @@
+import { getTimeZoneOffset } from './getTimeZoneOffset';
+
 import type { CalendarEvent, RawCalendarEvent } from './calendarEvents';
 
-const normaliseEvents = (events: RawCalendarEvent[], timezoneOffset: string): CalendarEvent[] => {
+const normaliseEvents = (events: RawCalendarEvent[]): CalendarEvent[] => {
   const beginingTime = new Date();
 
   beginingTime.setUTCHours(0);
@@ -9,20 +11,25 @@ const normaliseEvents = (events: RawCalendarEvent[], timezoneOffset: string): Ca
   beginingTime.setMilliseconds(0);
 
   const isoDateString = beginingTime.toISOString();
-  const timeAtZero = `${isoDateString.slice(isoDateString.indexOf(`T`), -1)}${timezoneOffset}`;
 
   return events.
-    map((item): CalendarEvent => ({
-      date: {
-        start: new Date('date' in item.start ? `${item.start.date}${timeAtZero}` : item.start.dateTime),
-        end: new Date('date' in item.end ? `${item.end.date}${timeAtZero}` : item.end.dateTime)
-      },
-      isWholeDayEvent: Boolean('date' in item.start),
-      content: {
-        ...Object.fromEntries(Object.entries(item).filter(([ key ]) =>
-          ![ 'end', 'start' ].includes(key))) as CalendarEvent['content']
-      }
-    }));
+    map((item): CalendarEvent => {
+      const start = 'date' in item.start ? item.start.date : item.start.dateTime;
+      const tzo = getTimeZoneOffset(new Date(start));
+      const timeAtZero = `${isoDateString.slice(isoDateString.indexOf(`T`), -1)}${tzo}`;
+
+      return {
+        date: {
+          start: new Date('date' in item.start ? `${item.start.date}${timeAtZero}` : item.start.dateTime),
+          end: new Date('date' in item.end ? `${item.end.date}${timeAtZero}` : item.end.dateTime)
+        },
+        isWholeDayEvent: Boolean('date' in item.start),
+        content: {
+          ...Object.fromEntries(Object.entries(item).filter(([ key ]) =>
+            ![ 'end', 'start' ].includes(key))) as CalendarEvent['content']
+        }
+      };
+    });
 };
 
 export {
